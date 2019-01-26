fb5dcfb1f6e31fb51ed07148600b04e248e6e287
teapotd <teapot404@gmail.com>
2019-01-26 15:06:07 +0100

    Add CudaArray init from size and fix tabs

--- include/cuda_helper.hpp
+++ include/cuda_helper.hpp
@@ -23,76 +23,81 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
 template<typename T>
 class CudaArray {
 public:
-	CudaArray() {}
-	CudaArray(const std::vector<T>& vec) { assign(vec); }
-	CudaArray(const CudaArray& other) = delete;
-	CudaArray& operator=(const CudaArray& other) = delete;
-	
-	~CudaArray() {
-		if (ptr) {
-			cudaFree(ptr);
-		}
-	}
+    CudaArray() {}
+    CudaArray(size_t n) { resizeDiscard(n); }
+    CudaArray(const std::vector<T>& vec) { assign(vec); }
+    CudaArray(const CudaArray& other) = delete;
+    CudaArray& operator=(const CudaArray& other) = delete;
+    
+    ~CudaArray() {
+        if (ptr) {
+            cudaFree(ptr);
+        }
+    }
 
-	void assign(const std::vector<T>& vec) {
-		if (!vec.empty()) {
-			if (cap < vec.size()) {
-				if (ptr) {
-					cudaFree(ptr);
-				}
-				cap = vec.size();
-				gpuErrchk(cudaMalloc(&ptr, cap*sizeof(T)));
-			}
-			gpuErrchk(cudaMemcpy(ptr, vec.data(), vec.size()*sizeof(T), cudaMemcpyHostToDevice));
-		}
-		len = vec.size();
-	}
+    void resizeDiscard(size_t n) {
+        if (cap < n) {
+            if (ptr) {
+                cudaFree(ptr);
+            }
+            cap = n;
+            gpuErrchk(cudaMalloc(&ptr, cap*sizeof(T)));
+        }
+        len = n;
+    }
 
-	void get(std::vector<T>& vec) const {
-		vec.resize(len);
-		gpuErrchk(cudaMemcpy(vec.data(), ptr, len*sizeof(T), cudaMemcpyDeviceToHost));
-	}
+    void assign(const std::vector<T>& vec) {
+        resizeDiscard(vec.size());
+        if (len > 0) {
+            gpuErrchk(cudaMemcpy(ptr, vec.data(), vec.size()*sizeof(T), cudaMemcpyHostToDevice));
+        }
+    }
 
-	T*       data()           { return ptr; }
-	const T* data()     const { return ptr; }
-	size_t   size()     const { return len; }
-	size_t   capacity() const { return cap; }
+    void get(std::vector<T>& vec) const {
+        vec.resize(len);
+        gpuErrchk(cudaMemcpy(vec.data(), ptr, len*sizeof(T), cudaMemcpyDeviceToHost));
+    }
+
+    T*       data()           { return ptr; }
+    const T* data()     const { return ptr; }
+    size_t   size()     const { return len; }
+    size_t   capacity() const { return cap; }
 
 private:
-	T *ptr{nullptr};
-	size_t len{0}, cap{0};
+    T *ptr{nullptr};
+    size_t len{0}, cap{0};
 };
 
 template<typename T>
 class CudaVar {
 public:
-	CudaVar() {
-		gpuErrchk(cudaMalloc(&ptr, sizeof(T)));
-	}
+    CudaVar() {
+        gpuErrchk(cudaMalloc(&ptr, sizeof(T)));
+    }
 
-	CudaVar(const T& elem) : CudaVar() {
-		set(elem);
-	}
+    CudaVar(const T& elem) : CudaVar() {
+        set(elem);
+    }
 
-	~CudaVar() {
-		cudaFree(ptr);
-	}
+    ~CudaVar() {
+        cudaFree(ptr);
+    }
 
-	CudaVar(const CudaVar& other) = delete;
-	CudaVar& operator=(const CudaVar& other) = delete;
+    CudaVar(const CudaVar& other) = delete;
+    CudaVar& operator=(const CudaVar& other) = delete;
 
-	void set(const T& elem) {
-		gpuErrchk(cudaMemcpy(ptr, &elem, sizeof(T), cudaMemcpyHostToDevice));
-	}
+    void set(const T& elem) {
+        gpuErrchk(cudaMemcpy(ptr, &elem, sizeof(T), cudaMemcpyHostToDevice));
+    }
 
-	T get() const {
-		T ret;
-		gpuErrchk(cudaMemcpy(&ret, ptr, sizeof(T), cudaMemcpyDeviceToHost));
-		return ret;
-	}
+    T get() const {
+        T ret;
+        gpuErrchk(cudaMemcpy(&ret, ptr, sizeof(T), cudaMemcpyDeviceToHost));
+        return ret;
+    }
 
-	T *pointer() { return ptr; }
+    T *pointer() { return ptr; }
 
 private:
-	T *ptr;
+    T *ptr;
 };
